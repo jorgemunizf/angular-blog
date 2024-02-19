b8706a0a34f51ca19fc669aab459012d2d882ad9
Jorge Muniz <jorge.munizfilho@gmail.com>
2024-02-18 23:36:39 -0300

    feat(componetns): Criado propriedades de componentes dinamicos

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover: String = '';
+  @Input()
+  cardTitle: String = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
